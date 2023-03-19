3303f30bd16eccec1c199db5cb90f5038d3378fa
Lene Signorelli <lenesignorelli@yahoo.com.br>
2023-03-19 15:16:33 -0300

    Parte 4 disponibiliza os itens do array e cria proptypes

--- src/components/Selecao/index.js
+++ src/components/Selecao/index.js
@@ -1,16 +1,24 @@
+import PropTypes from 'prop-types';
 import './styles.css'
 
-export default function Selecao({siglaPais, nomePais, grupoPais}) {
+function Selecao({siglaPais, nomePais, grupoPais}) {
     // console.log(props);
     // props.siglaPais
     // props.nomePais
     // props.grupoPais
     // console.log(nomePais);
   return (
-    <div>
+    <div className='linha-selecao'>
         <span>{nomePais}</span>
         <span>{siglaPais}</span>
         <span>{grupoPais}</span>
     </div>
   )
 }
+Selecao.propTypes= {
+    nomePais: PropTypes.string.isRequired,
+    siglaPais: PropTypes.string.isRequired,
+    grupoPais: PropTypes.string.isRequired
+}
+
+export default Selecao;
