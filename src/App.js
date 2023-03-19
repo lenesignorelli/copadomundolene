08397ad461f5802278359410bd490aedffa58627
Lene Signorelli <lenesignorelli@yahoo.com.br>
2023-03-19 11:36:24 -0300

    Parte 2 cria uma constante contendo o array com as selecoes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,41 @@
 
 import './App.css';
 
+const selecoes = [
+  { nome: "Qatar", sigla: "QAT", grupo: "A"},
+  { nome: "Equador", sigla: "ECU", grupo: "A"},
+  { nome: "Senegal", sigla: "SEN", grupo: "A"},
+  { nome: "Holanda", sigla: "NED", grupo: "A"},
+  { nome: "Inglaterra", sigla: "ENG", grupo: "B"},
+  { nome: "Irã", sigla: "IRN", grupo: "B"},
+  { nome: "Estados Unidos", sigla: "USA", grupo: "B"},
+  { nome: "País de Gales", sigla: "WAL", grupo: "B"},
+  { nome: "Argentina", sigla: "ARG", grupo: "C"},
+  { nome: "Arábia Saudita", sigla: "KSA", grupo: "C"},
+  { nome: "México", sigla: "MEX", grupo: "C"},
+  { nome: "Polônia", sigla: "POL", grupo: "C"},
+  { nome: "França", sigla: "FRA", grupo: "D"},
+  { nome: "Dinamarca", sigla: "DEN", grupo: "D"},
+  { nome: "Tunísia", sigla: "TUN", grupo: "D"},
+  { nome: "Austrália", sigla: "AUS", grupo: "D"},
+  { nome: "Espanha", sigla: "ESP", grupo: "E"},
+  { nome: "Alemanha", sigla: "GER", grupo: "E"},
+  { nome: "Japão", sigla: "JPN", grupo: "E"},
+  { nome: "Costa Rica", sigla: "CRC", grupo: "E"},
+  { nome: "Bélgica", sigla: "BEL", grupo: "F"},
+  { nome: "Canadá", sigla: "CAN", grupo: "F"},
+  { nome: "Marrocos", sigla: "MAR", grupo: "F"},
+  { nome: "Croácia", sigla: "CRO", grupo: "F"},
+  { nome: "Brasil", sigla: "BRA", grupo: "G"},
+  { nome: "Sérvia", sigla: "SRB", grupo: "G"},
+  { nome: "Suíça", sigla: "SUI", grupo: "G"},
+  { nome: "Camarões", sigla: "CMR", grupo: "G"},
+  { nome: "Portugal", sigla: "POR", grupo: "H"},
+  { nome: "Gana", sigla: "GHA", grupo: "H"},
+  { nome: "Uruguai", sigla: "URU", grupo: "H"},
+  { nome: "Coréia do Sul", sigla: "KOR", grupo: "H"}
+  ]
+
 function App() {
   return (
     <div className="App">
